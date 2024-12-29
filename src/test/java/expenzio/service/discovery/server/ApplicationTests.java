8687d59d58a2072693be3a2a7814486d2c735a54
Aditya Bhosale <devadviam@gmail.com>
2024-12-29 13:16:29 +0530

    base files for Service Discovery server

---
+++ src/test/java/expenzio/service/discovery/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package expenzio.service.discovery.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
